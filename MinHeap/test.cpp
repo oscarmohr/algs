aa465a3f5d7d2c1373fbc1a10375209339ee6600
Oscar Mohr <oscar.mohr@outlook.com>
2022-04-21 19:33:30 +0200

    added array initialization test

--- MinHeap/test.cpp
+++ MinHeap/test.cpp
@@ -4,11 +4,13 @@
 using namespace util;
 
 void fail(string msg="") {
-  print("[ FAIL ] test failed.", msg);
+  put("[ FAIL ] test failed.");
+  print(msg == "" ? msg : " " + msg);
 }
 
 void pass(string msg="") {
-  print("[ PASSED ] test passed.", msg);
+  put("[ PASSED ] test passed.");
+  print(msg == "" ? msg : " " + msg);
 }
 
 auto test_decreasing(auto h) {
@@ -31,7 +33,7 @@ auto test_decreasing(auto h) {
 }
 
 auto test_large_cardinality(auto h) {
-  auto N = 1e4;
+  auto N = 1e8;
   auto good = true;
 
   print("[ TEST ] testing large cardinality of ", N);
@@ -45,9 +47,46 @@ auto test_large_cardinality(auto h) {
       fail("incorrect minimum.");
       break;
     }
-    h.del();
+    h.pop_min();
   }
 
+  good ? pass() : fail();
+  return good;
+}
+
+auto test_array_initialization() {
+  auto N = 1e4;
+  auto good = true;
+
+
+  print("[ TEST ] testing array initialization of ", N, " random elements");
+
+  auto tmp = new int[N];
+
+  for (auto i = 0; i < N; i++)
+    tmp[i] = std::rand();
+
+  auto h = MinHeap(tmp, N);
+
+  delete[] tmp;
+
+  print("Heap says it is a heap: ", h.is_heap() ? "true" : "false");
+
+  auto ref = std::vector<int>();
+
+  for (auto i = 0; i < N; i++) {
+    ref.push_back(h.min());
+    h.pop_min();
+  }
+
+  for (auto i = 0; i < N - 1; i++) {
+    if (ref[i] > ref[i + 1]) {
+      good = false;
+      fail("unsorted.");
+      break;
+    }
+  }
+  
   good ? pass() : fail();
   return good;
 }
@@ -67,4 +106,5 @@ auto test(auto t, const auto& args...) {
 int main() {
   test_decreasing(MinHeap(50));
   test_large_cardinality(MinHeap(12));
+  test_array_initialization();
 }
